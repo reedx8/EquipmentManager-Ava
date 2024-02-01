5b1cbf7df78f949aa126ab3a49bb32b5855353c1
Xavier Reed <xreed18@gmail.com>
2024-02-01 15:30:00 -0800

    commented out code deleted

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,13 +24,3 @@ export default function RootLayout({ children }) {
     </html>
   )
 }
-
-/*
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
-}
-*/
