11fb535505b6303afe91ad739fd5f77d76a7cf12
Xavier Reed <xreed18@gmail.com>
2024-02-12 16:42:30 -0800

    header renders page name

--- src/app/Components/header.jsx
+++ src/app/Components/header.jsx
@@ -1,10 +1,37 @@
-import styles from '../styles/header.module.css'
+'use client';
+import { useEffect, useState } from 'react';
+import styles from '../styles/header.module.css';
+import { usePathname } from 'next/navigation';
 
-export default function Header(){
+export default function Header() {
+    const [pathname, setPathname] = useState('');
+    let name = usePathname();
+
+    useEffect(() => {
+        if (name == '/') {
+            setPathname('Dashboard');
+        } else {
+            name = name.slice(1);
+            name = name.charAt(0).toUpperCase() + name.slice(1);
+            setPathname(name);
+        }
+    }, [name]);
+
+    /*
+    let pathName = usePathname();
+
+    if (pathName == '/') {
+        pathName = 'Dashboard';
+    } else {
+        pathName = usePathname().slice(1);
+        pathName = pathName.charAt(0).toUpperCase() + pathName.slice(1);
+    }
+    */
+
     return (
         <div className={styles.main}>
-            <p>Home</p>
+            <p>{pathname}</p>
             <p>AVA Roasteria</p>
         </div>
-    )
+    );
 }
