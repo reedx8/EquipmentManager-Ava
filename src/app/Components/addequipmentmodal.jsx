5b1d92a47fc15257470f5eb66d253654e1cf033d
Xavier Reed <xreed18@gmail.com>
2024-03-29 14:58:42 -0700

    add equipment button now updates db

--- src/app/Components/addequipmentmodal.jsx
+++ src/app/Components/addequipmentmodal.jsx
@@ -3,9 +3,40 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import styles from '../equipment/equipment.module.css';
+import supabase from '../config/supabaseClient';
 
 // Adds equipment to the 'equipment' table in database
 export default function AddEquipmentModal(props) {
+    const [equipmentName, setEquipmentName] = useState('');
+    const [equipmentType, setEquipmentType] = useState('Tablet'); // First option in select
+    const [storeLocation, setStoreLocation] = useState('Hall');
+    const [provider, setProvider] = useState('Ava Roasteria');
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        /*
+        alert(
+            `Name: ${equipmentName}, Type: ${equipmentType}, Location: ${storeLocation}, Provider: ${provider}`
+        );
+        */
+
+        const { data, error } = await supabase.from('Equipment').insert([
+            {
+                Name: equipmentName,
+                Equip_Type: equipmentType,
+                Store_Name: storeLocation,
+                Provider_Name: provider,
+            },
+        ]);
+
+        if (error) {
+            alert('Error adding equipment: ' + error.message);
+        } else {
+            props.closeModal();
+        }
+    };
+
     return (
         <div className={styles.modal}>
             <div className={styles.modalBackground}>
@@ -13,42 +44,67 @@ export default function AddEquipmentModal(props) {
                     <h1>Add Equipment</h1>
                 </div>
                 {/* TODO: Add form validation, and generate content from DB instead */}
-                <form className={styles.modalContents}>
+                <form className={styles.modalContents} onSubmit={handleSubmit}>
                     <div>
-                        <label>Name</label>
-                        <input type='text' name='equipmentName' />
+                        <label htmlFor='equipmentName'>Name</label>
+                        <input
+                            id='equipmentName'
+                            type='text'
+                            name='equipmentName'
+                            value={equipmentName}
+                            onChange={(e) => setEquipmentName(e.target.value)}
+                        />
                     </div>
                     <div>
-                        <label for='equipmentType'>Type</label>
-                        <select id='equipmentType' name='equipmentType'>
-                            <option value='tablet'>Tablet</option>
-                            <option value='computer'>Computer</option>
-                            <option value='printer'>Printer</option>
-                            <option value='modem'>Modem</option>
-                            <option value='oven'>Oven</option>
-                            <option value='refrigerator'>Refrigerator</option>
-                            <option value='espresso'>Espresso Machine</option>
-                            <option value='microwave'>Microwave</option>
+                        <label htmlFor='equipmentType'>Type</label>
+                        <select
+                            id='equipmentType'
+                            name='equipmentType'
+                            value={equipmentType}
+                            onChange={(e) => setEquipmentType(e.target.value)}
+                        >
+                            <option value='Tablet'>Tablet</option>
+                            <option value='Computer'>Computer</option>
+                            <option value='Printer'>Printer</option>
+                            <option value='Network Device'>
+                                Network Device
+                            </option>
+                            <option value='Oven'>Oven</option>
+                            <option value='Refrigerator'>Refrigerator</option>
+                            <option value='Espresso Machine'>
+                                Espresso Machine
+                            </option>
+                            <option value='Microwave'>Microwave</option>
                         </select>
                     </div>
                     <div>
-                        <label for='storeLocation'>Store Location</label>
-                        <select id='storeLocation' name='storeLocation'>
-                            <option value='hall'>Hall</option>
-                            <option value='barrows'>Barrows</option>
-                            <option value='orenco'>Orenco</option>
-                            <option value='kruse'>Kruse</option>
+                        <label htmlFor='storeLocation'>Store Location</label>
+                        <select
+                            id='storeLocation'
+                            name='storeLocation'
+                            value={storeLocation}
+                            onChange={(e) => setStoreLocation(e.target.value)}
+                        >
+                            <option value='Hall'>Hall</option>
+                            <option value='Barrows'>Barrows</option>
+                            <option value='Orenco'>Orenco</option>
+                            <option value='Kruse'>Kruse</option>
                         </select>
                     </div>
                     <div>
-                        <label for='provider'>Provider</label>
-                        <select id='provider' name='provider'>
-                            <option value='ava'>Ava Roasteria</option>
-                            <option value='grubhub'>Grubhub</option>
-                            <option value='doordash'>DoorDash</option>
-                            <option value='ubereats'>Uber Eats</option>
-                            <option value='comcast'>Comcast</option>
-                            <option value='revel'>Revel</option>
+                        <label htmlFor='provider'>Provider</label>
+                        <select
+                            id='provider'
+                            name='provider'
+                            value={provider}
+                            onChange={(e) => setProvider(e.target.value)}
+                        >
+                            <option value='Ava Roasteria'>Ava Roasteria</option>
+                            <option value='Grubhub'>Grubhub</option>
+                            <option value='Doordash'>DoorDash</option>
+                            <option value='Uber Eats'>Uber Eats</option>
+                            <option value='Comcast'>Comcast</option>
+                            <option value='Revel'>Revel</option>
                         </select>
                     </div>
                     <div className={styles.buttonsRow}>
@@ -59,11 +115,7 @@ export default function AddEquipmentModal(props) {
                         >
                             Close
                         </button>
-                        <button
-                            className={styles.modalButtons}
-                            onClick={() => props.closeModal()}
-                            type='button'
-                        >
+                        <button className={styles.modalButtons} type='submit'>
                             Add
                         </button>
                     </div>
